8d26f00f0749fd962a16236d4783dab2548eeb5f
qexat <contact@qexat.com>
2024-03-07 18:14:33 +0100

    add message dictionary typed dict (#11)
    * add message dictionary typed dict
      * provides message autocomplete!
      * defensive programming to avoid updating somewhere and forgetting something anywhere else :')

--- reload_wifi_py/messages/__init__.py
+++ reload_wifi_py/messages/__init__.py
@@ -1,11 +1,36 @@
 import json
 import locale
 import os
+import typing
 
 
 __all__ = ["MESSAGES"]
 
 DEFAULT_LANG = "en_US"
+
+
+class MessageDictionary(typing.TypedDict):
+    """
+    Dictionary representing every possible message.
+
+    Static type checkers can catch typos in keys or unexisting ones.
+    """
+
+    attempts_report_template: str
+    cant_establish_template: str
+    dry_run_mode: str
+    error_value_nan: str
+    error_value_inf: str
+    flag_note_template: str
+    ignored_failure: str
+    no_wifi_established: str
+    note_instant_disconnection: str
+    reset_anyway: str
+    restart_failure: str
+    started_restarting: str
+    user_exit_requested: str
+    wifi_already_established_template: str
+    wifi_established_template: str
 
 
 def get_file_path(lang: str) -> str:
@@ -24,18 +49,34 @@ def get_file_path(lang: str) -> str:
     return path
 
 
-def fix_messages(source: dict[str, str]) -> dict[str, str]:
+def fix_missing_translations(source: dict[str, str]) -> MessageDictionary:
     """
-    Add original english messages for missing translations.
+    Add default messages for missing translations.
     """
 
     with open(get_file_path(DEFAULT_LANG)) as default_messages_json:
         default_messages = json.load(default_messages_json)
 
-    return default_messages | source
+    # ensure that the default dictionary contains only and all messages
+    # if we add more messages in the future, it will make sure we don't forget
+    # something somewhere :')
+    assert is_dictionary_complete(default_messages)
+
+    return typing.cast(MessageDictionary, default_messages | source)
+
+
+def is_dictionary_complete(
+    dictionary: dict[str, str],
+) -> typing.TypeGuard[MessageDictionary]:
+    """
+    Check whether a dictionary is complete, i.e. supports every possible
+    message.
+    """
+
+    return MessageDictionary.__annotations__.keys() == dictionary.keys()
 
 
 MESSAGES_FILE_PATH = get_file_path(locale.getlocale()[0] or DEFAULT_LANG)
 
 with open(MESSAGES_FILE_PATH, "r") as messages_json:
-    MESSAGES = fix_messages(json.load(messages_json))
+    MESSAGES = fix_missing_translations(json.load(messages_json))
